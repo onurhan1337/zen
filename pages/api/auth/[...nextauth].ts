f1989065a4f5e27cf4ef98173c9eeba581f980c6
Onurhan <onurhan@getutilify.com>
2023-10-20 18:25:21 +0300

    fix: next config for google avatar img

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
