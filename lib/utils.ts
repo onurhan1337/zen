582e57181605000774662a47e6d790a42ee176f8
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2023-12-25 10:13:55 +0300

    fix: delete unused fetcher in utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -13,28 +13,6 @@ export const timeAgo = (timestamp: Date, timeOnly?: boolean): string => {
   }`;
 };
 
-export async function fetcher<JSON = any>(
-  input: RequestInfo,
-  init?: RequestInit,
-): Promise<JSON> {
-  const res = await fetch(input, init);
-
-  if (!res.ok) {
-    const json = await res.json();
-    if (json.error) {
-      const error = new Error(json.error) as Error & {
-        status: number;
-      };
-      error.status = res.status;
-      throw error;
-    } else {
-      throw new Error("An unexpected error occurred");
-    }
-  }
-
-  return res.json();
-}
-
 export function nFormatter(num: number, digits?: number) {
   if (!num) return "0";
   const lookup = [
