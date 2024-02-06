2032919b4725f73e314ec3227e78270a8a117a38
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-02-06 13:13:26 +0300

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,39 +1,46 @@
-import { Suspense } from "react";
-import { SessionProvider } from "next-auth/react";
-import { Session } from "next-auth";
-import type { AppProps } from "next/app";
+import {Analytics} from '@vercel/analytics/react';
+
+import {Suspense} from "react";
+import {SessionProvider} from "next-auth/react";
+import {Session} from "next-auth";
+import type {AppProps} from "next/app";
 import "../styles/globals.css";
 import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
+import {Theme} from "@radix-ui/themes";
 import NavBar from "@/components/layout/navbar";
-import { Toaster } from "sonner";
-import { Inter } from 'next/font/google'
+import {Toaster} from "sonner";
+import {Inter} from 'next/font/google'
 
 const inter = Inter({
-  subsets: ['latin']
+    subsets: ['latin']
 })
 
 export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps<{ session: Session }>) {
-  return (
-    <SessionProvider session={session}>
-      <Theme
-        appearance={'dark'}
-      >
-        <header>
-          <Suspense fallback="...">
-            <NavBar />
-          </Suspense>
-        </header>
-        <main
-          className={`${inter.className} import mx-5 py-32`}
-        >
-          <Component {...pageProps} />
-        </main>
-        <Toaster richColors closeButton={true} />
-      </Theme>
-    </SessionProvider>
-  );
+                                Component,
+                                pageProps: {session, ...pageProps},
+                            }: AppProps<{
+    session: Session
+}>) {
+    return (
+        <>
+            <SessionProvider session={session}>
+                <Theme
+                    appearance={'dark'}
+                >
+                    <header>
+                        <Suspense fallback="...">
+                            <NavBar/>
+                        </Suspense>
+                    </header>
+                    <main
+                        className={`${inter.className} import mx-5 py-32`}
+                    >
+                        <Component {...pageProps} />
+                    </main>
+                    <Toaster richColors closeButton={true}/>
+                </Theme>
+            </SessionProvider>
+            <Analytics/>
+        </>
+    );
 }
