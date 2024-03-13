65a1581c6808e5f6956844efc6c3a90d168b64e2
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-03-13 15:48:38 +0300

    refac: scrollbar in members list

--- components/projects/board/membersList.tsx
+++ components/projects/board/membersList.tsx
@@ -1,12 +1,12 @@
-import {Box, Button, Dialog, Text} from '@radix-ui/themes';
-import {useSession} from "next-auth/react";
-import useSWR, {mutate} from "swr";
-import {Project, User} from "@prisma/client";
-import fetcher from "@/lib/fetcher";
 import {LoadingSpinner} from "@/components/shared/icons";
-import {toast} from "sonner";
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
+import fetcher from "@/lib/fetcher";
+import {Project, User} from "@prisma/client";
+import {Box, Button, Dialog, ScrollArea, Text} from "@radix-ui/themes";
 import {Check, Inbox, Users, X} from "lucide-react";
+import {useSession} from "next-auth/react";
+import {toast} from "sonner";
+import useSWR, {mutate} from "swr";
 
 interface MembersListDialogProps {
     projectId: string;
@@ -14,32 +14,28 @@ interface MembersListDialogProps {
     setOpen: (open: boolean) => void;
 }
 
-const MembersListDialog = ({projectId, isOpen, setOpen}: MembersListDialogProps) => {
+const MembersListDialog = ({
+                               projectId, isOpen, setOpen,
+                           }: MembersListDialogProps) => {
     const {data: session} = useSession();
     const {data: project} = useSWR<Project & {
-        owners: User[],
-        members: User[],
-        inviteCode: string,
+        owners: User[]; members: User[]; inviteCode: string;
     }>(`/api/project/${projectId}/member`, fetcher, {
         revalidateOnFocus: true,
     });
 
     if (!project) {
-        return (
-            <div className={"flex flex-col items-center gap-2"}>
-                <LoadingSpinner/>
-            </div>
-        );
+        return (<div className={"flex flex-col items-center gap-2"}>
+            <LoadingSpinner/>
+        </div>);
     }
 
     const onRemoveMember = async (userId: string) => {
         try {
             const data = await fetcher(`/api/project/${projectId}/member`, {
-                method: "DELETE",
-                headers: {
+                method: "DELETE", headers: {
                     "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
+                }, body: JSON.stringify({
                     userId,
                 }),
             });
@@ -59,75 +55,79 @@ const MembersListDialog = ({projectId, isOpen, setOpen}: MembersListDialogProps)
         }
     };
 
-    return (
-        <Dialog.Root open={isOpen} onOpenChange={(open: boolean) => setOpen(open)}>
-            <Dialog.Trigger>
-                <Button
-                    color={'gray'}
-                    variant={'soft'}
-                    onClick={() => setOpen(true)}>
-                    <Users className="h-4 w-4"/>
-                </Button>
-            </Dialog.Trigger>
-            <Dialog.Content
-                style={{maxWidth: 450}}
-            >
-                <Dialog.Title>
-                    Members List
-                </Dialog.Title>
-                <Dialog.Description>
-                    List of members in this project.
-                </Dialog.Description>
-                <Box mt={'4'}>
-                    <Tabs defaultValue="members"
+    return (<Dialog.Root open={isOpen} onOpenChange={(open: boolean) => setOpen(open)}>
+        <Dialog.Trigger>
+            <Button color={"gray"} variant={"soft"} onClick={() => setOpen(true)}>
+                <Users className="h-4 w-4"/>
+            </Button>
+        </Dialog.Trigger>
+        <Dialog.Content
+            style={{
+                maxWidth: 450, minHeight: 400,
+            }}
+        >
+            <Dialog.Title>Members List</Dialog.Title>
+            <Dialog.Description>
+                List of members in this project.
+            </Dialog.Description>
+            <Box mt={"4"}>
+                <Tabs defaultValue="members">
+                    <TabsList
+                        className={"flex w-full max-w-screen-sm items-center justify-center"}
                     >
-                        <TabsList className={'flex w-full max-w-screen-sm items-center justify-center'}>
-                            <TabsTrigger className={'w-full'} value="members">
-                                Members
-                            </TabsTrigger>
-                            <TabsTrigger className={'w-full'} value="pendingInvites">
-                                Pending Invites
-                            </TabsTrigger>
-                        </TabsList>
-                        <TabsContent value={'members'}>
+                        <TabsTrigger className={"w-full"} value="members">
+                            Members
+                        </TabsTrigger>
+                        <TabsTrigger className={"w-full"} value="pendingInvites">
+                            Pending Invites
+                        </TabsTrigger>
+                    </TabsList>
+                    <TabsContent value={"members"} className="overflow-hidden">
+                        <ScrollArea
+                            type="always"
+                            scrollbars="vertical"
+                            style={{height: 180}}
+                        >
                             <div className="text-sm text-zinc-400">
-                                <ul className="h-[200px] w-full list-inside list-disc overflow-y-auto py-4">
-                                    {project && ((project.members && project.members.length > 0) || (project.owners && project.owners.length > 0)) ? (
-                                        [...(project.members || []), ...(project.owners || [])].map((user) => (
-                                            <li
-                                                className="flex flex-row items-center justify-between py-3 text-sm text-zinc-500"
-                                                key={user.email}
-                                            >
-                                                <div className="flex flex-col items-start">
-                                    <span className={"text-zinc-400"}>{user.name} {
-                                        session && session.user.email === user.email && '(You)'
-                                    }</span>
-                                                    <span className={"text-zinc-500"}>{user.email}</span>
-                                                </div>
-                                                {session && session.user.email !== user.email && (
-                                                    <button onClick={() => onRemoveMember(user.id)}>x</button>
-                                                )}
-                                            </li>
-                                        ))
-                                    ) : (
+                                <ul className="h-[200px] w-full list-inside list-disc pr-2">
+                                    {project && ((project.members && project.members.length > 0) || (project.owners && project.owners.length > 0)) ? ([...(project.members || []), ...(project.owners || []),].map((user) => (
                                         <li
-                                            key="no-members-found"
-                                            className="text-center text-sm text-zinc-400 "
+                                            className="flex flex-row items-center justify-between p-3 text-sm text-zinc-500"
+                                            key={user.email}
                                         >
-                                            No members found.
-                                        </li>
-                                    )}
+                                            <div className="flex flex-col items-start">
+                            <span className={"text-zinc-400"}>
+                              {user.name}{" "}
+                                {session && session.user.email === user.email && "(You)"}
+                            </span>
+                                                <span className={"text-zinc-500"}>
+                              {user.email}
+                            </span>
+                                            </div>
+                                            {session && session.user.email !== user.email && (
+                                                <button onClick={() => onRemoveMember(user.id)}>
+                                                    x
+                                                </button>)}
+                                        </li>))) : (<li
+                                        key="no-members-found"
+                                        className="text-center text-sm text-zinc-400 "
+                                    >
+                                        No members found.
+                                    </li>)}
                                 </ul>
                             </div>
-                        </TabsContent>
-                        <TabsContent value="pendingInvites">
-                            <PendingInvites code={project.inviteCode!} projectId={projectId}/>
-                        </TabsContent>
-                    </Tabs>
-                </Box>
-            </Dialog.Content>
-        </Dialog.Root>
-    );
+                        </ScrollArea>
+                    </TabsContent>
+                    <TabsContent value="pendingInvites">
+                        <PendingInvites
+                            code={project.inviteCode!}
+                            projectId={projectId}
+                        />
+                    </TabsContent>
+                </Tabs>
+            </Box>
+        </Dialog.Content>
+    </Dialog.Root>);
 };
 
 export default MembersListDialog;
@@ -139,33 +139,33 @@ interface ProjectInviteAttempt {
     userId: string;
     projectId: string;
     expires: Date | null;
-    role: 'member' | 'owner';
+    role: "member" | "owner";
 }
 
-const PendingInvites = ({code, projectId}: {code: string, projectId: string; }) => {
-    const {data: pendingInvites} = useSWR<ProjectInviteAttempt[]>(`/api/project/${projectId}/member/invite`, fetcher);
+const PendingInvites = ({
+                            code, projectId,
+                        }: {
+    code: string; projectId: string;
+}) => {
+    const {data: pendingInvites} = useSWR<ProjectInviteAttempt[]>(`/api/project/${projectId}/member/invite`, fetcher,);
 
     if (!pendingInvites || pendingInvites.length === 0) {
-        return (
-            <div className="flex w-full flex-col items-center justify-center space-y-2 my-4">
-                <Inbox className="mx-auto h-8 w-8 text-zinc-400"/>
-                <Text>No pending invites found.</Text>
-            </div>
-        );
+        return (<div className="my-4 flex w-full flex-col items-center justify-center space-y-2">
+            <Inbox className="mx-auto h-8 w-8 text-zinc-400"/>
+            <Text>No pending invites found.</Text>
+        </div>);
     }
 
-    const onAcceptInvite = async ({code, userId, projectId,}: {
-        code: string;
-        userId: string;
-        projectId: string;
+    const onAcceptInvite = async ({
+                                      code, userId, projectId,
+                                  }: {
+        code: string; userId: string; projectId: string;
     }) => {
         try {
             const res = await fetch(`/api/project/${projectId}/member/invite`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({code, userId, projectId})
+                method: "POST", headers: {
+                    "Content-Type": "application/json",
+                }, body: JSON.stringify({code, userId, projectId}),
             });
 
             if (res.ok) {
@@ -180,19 +180,18 @@ const PendingInvites = ({code, projectId}: {code: string, projectId: string; }) 
             console.error("Error accepting invite:", error);
             toast.error("Something went wrong!");
         }
-    }
+    };
 
-    const onDeclineInvite = async ({userId, projectId,}: {
-        userId: string;
-        projectId: string;
+    const onDeclineInvite = async ({
+                                       userId, projectId,
+                                   }: {
+        userId: string; projectId: string;
     }) => {
         try {
             const res = await fetch(`/api/project/${projectId}/member/invite`, {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({userId, projectId})
+                method: "DELETE", headers: {
+                    "Content-Type": "application/json",
+                }, body: JSON.stringify({userId, projectId}),
             });
 
             if (res.ok) {
@@ -206,54 +205,43 @@ const PendingInvites = ({code, projectId}: {code: string, projectId: string; }) 
             console.error("Error declining invite:", error);
             toast.error("Something went wrong!");
         }
+    };
 
-    }
-
-    return (
-        <div className="text-sm text-zinc-400">
-            <ul className="h-[200px] w-full list-inside list-disc overflow-y-auto py-4">
-                {pendingInvites.map((user) => (
-                    <li
-                        className="flex flex-row items-center justify-between py-3 text-sm text-zinc-500"
-                        key={user.userId}
-                    >
-                        <div className="w-full flex flex-row items-center justify-between">
-                            <div
-                                className="flex flex-col items-start"
-                            >
-                                <span className={"text-zinc-400"}>{user.name}</span>
-                                <span className={"text-zinc-500"}>{user.email}</span>
-                            </div>
-                            <div className="flex gap-2">
-                                <Button
-                                    size={'1'}
-                                    color={'lime'}
-                                    variant={'outline'}
-                                    onClick={() => onAcceptInvite({
-                                        code: code,
-                                        userId: user.userId,
-                                        projectId,
-                                    })}
-                                >
-                                    <Check className="h-4 w-4"/>
-                                </Button>
-                                <Button
-                                    size={'1'}
-                                    color={'red'}
-                                    variant={'outline'}
-                                    onClick={() => onDeclineInvite({
-                                        userId: user.userId,
-                                        projectId,
-                                    })
-                                        }
-                                >
-                                    <X className="h-4 w-4"/>
-                                </Button>
-                            </div>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-}
+    return (<div className="text-sm text-zinc-400">
+        <ul className="h-[200px] w-full list-inside list-disc overflow-y-auto py-4">
+            {pendingInvites.map((user) => (<li
+                className="flex flex-row items-center justify-between py-3 text-sm text-zinc-500"
+                key={user.userId}
+            >
+                <div className="flex w-full flex-row items-center justify-between">
+                    <div className="flex flex-col items-start">
+                        <span className={"text-zinc-400"}>{user.name}</span>
+                        <span className={"text-zinc-500"}>{user.email}</span>
+                    </div>
+                    <div className="flex gap-2">
+                        <Button
+                            size={"1"}
+                            color={"lime"}
+                            variant={"outline"}
+                            onClick={() => onAcceptInvite({
+                                code: code, userId: user.userId, projectId,
+                            })}
+                        >
+                            <Check className="h-4 w-4"/>
+                        </Button>
+                        <Button
+                            size={"1"}
+                            color={"red"}
+                            variant={"outline"}
+                            onClick={() => onDeclineInvite({
+                                userId: user.userId, projectId,
+                            })}
+                        >
+                            <X className="h-4 w-4"/>
+                        </Button>
+                    </div>
+                </div>
+            </li>))}
+        </ul>
+    </div>);
+};
