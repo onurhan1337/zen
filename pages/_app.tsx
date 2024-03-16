90e08cdab31b9cba166d0a2e50793623e9016735
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-03-16 20:22:19 +0300

    fix: update dependencies and add accordion component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,46 +1,41 @@
-import {Analytics} from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
-import {Suspense} from "react";
-import {SessionProvider} from "next-auth/react";
-import {Session} from "next-auth";
-import type {AppProps} from "next/app";
-import "../styles/globals.css";
-import "@radix-ui/themes/styles.css";
-import {Theme} from "@radix-ui/themes";
 import NavBar from "@/components/layout/navbar";
-import {Toaster} from "sonner";
-import {Inter} from 'next/font/google'
+import { Toaster } from "@/components/ui/sonner";
+import { Theme } from "@radix-ui/themes";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+import { Suspense } from "react";
+import "../styles/globals.css";
 
 const inter = Inter({
-    subsets: ['latin']
-})
+  subsets: ["latin"],
+});
 
 export default function App({
-                                Component,
-                                pageProps: {session, ...pageProps},
-                            }: AppProps<{
-    session: Session
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{
+  session: Session;
 }>) {
-    return (
-        <>
-            <SessionProvider session={session}>
-                <Theme
-                    appearance={'dark'}
-                >
-                    <header>
-                        <Suspense fallback="...">
-                            <NavBar/>
-                        </Suspense>
-                    </header>
-                    <main
-                        className={`${inter.className} import mx-5 py-32`}
-                    >
-                        <Component {...pageProps} />
-                    </main>
-                    <Toaster richColors closeButton={true}/>
-                </Theme>
-            </SessionProvider>
-            <Analytics/>
-        </>
-    );
+  return (
+    <>
+      <SessionProvider session={session}>
+        <Theme appearance={"dark"}>
+          <header>
+            <Suspense fallback="...">
+              <NavBar />
+            </Suspense>
+          </header>
+          <main className={`${inter.className} mx-5 py-32`}>
+            <Component {...pageProps} />
+          </main>
+          <Toaster richColors closeButton={true} />
+        </Theme>
+      </SessionProvider>
+      <Analytics />
+    </>
+  );
 }
