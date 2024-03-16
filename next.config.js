0d9384341db383596d5b90db95caca6df70cd2b2
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-03-16 20:24:22 +0300

    fix: remove unnecessary environment variables from next.config.js

--- next.config.js
+++ next.config.js
@@ -10,20 +10,6 @@ const nextConfig = {
       "github.com",
     ],
   },
-  transpilePackages: ["geist"],
-  env: {
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    NEXT_AUTH_SECRET: process.env.NEXT_AUTH_SECRET,
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-    DIRECT_DATABASE_URL: process.env.DIRECT_DATABASE_URL,
-    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
-    POSTGRES_USER: process.env.POSTGRES_USER,
-    POSTGRES_HOST: process.env.POSTGRES_HOST,
-    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
-    POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
-  },
 };
 
 module.exports = nextConfig;
