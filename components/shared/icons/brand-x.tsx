dd82f66bd62ad57691684890c0c467d03a8ac143
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-04-15 23:28:19 +0300

    refac: update twitter icon with x brand

---
+++ components/shared/icons/brand-x.tsx
@@ -0,0 +1,18 @@
+
+export default function BrandX({ className }: { className?: string }) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24" 
+            fill="none" 
+            stroke="currentColor" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+            className="icon icon-tabler icons-tabler-outline icon-tabler-brand-x">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 4l11.733 16h4.267l-11.733 -16z" />
+                <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
+        </svg>
+    );
+}
