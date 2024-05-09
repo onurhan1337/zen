6059e975d9e5054f2743031d14378405ec0e7242
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-05-09 23:06:37 +0300

    feat: add animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,10 +22,29 @@ const config: Config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: '0' },
         },
+        shimmer: {
+          "0%, 90%, 100%": {
+            "background-position": "calc(-100% - var(--shimmer-width)) 0",
+          },
+          "30%, 60%": {
+            "background-position": "calc(100% + var(--shimmer-width)) 0",
+          },
+        },
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "shimmer": "shimmer 8s infinite",
+        marquee: "marquee var(--duration) linear infinite",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
       },
       fontFamily: {
         sans: ["Geist", "sans-serif"],
